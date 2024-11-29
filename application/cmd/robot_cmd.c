1a43baf631da2c0fb1b7d9a4d58e8270037d4e7e
NeptuneIsTheBest <13058097081a@gmail.com>
2024-11-29 14:45:11 +0800

    style: 重新格式化部分代码

--- application/cmd/robot_cmd.c
+++ application/cmd/robot_cmd.c
@@ -6,43 +6,42 @@
 #include "ins_task.h"
 #include "master_process.h"
 #include "message_center.h"
-#include "general_def.h"
 #include "dji_motor.h"
 #include "bmi088.h"
 // bsp
-#include "bsp_dwt.h"
 #include "bsp_log.h"
 
 // 私有宏,自动将编码器转换成角度值
 #define YAW_ALIGN_ANGLE (YAW_CHASSIS_ALIGN_ECD * ECD_ANGLE_COEF_DJI) // 对齐时的角度,0-360
-#define PTICH_HORIZON_ANGLE (PITCH_HORIZON_ECD * ECD_ANGLE_COEF_DJI) // pitch水平时电机的角度,0-360
+#define PITCH_HORIZON_ANGLE (PITCH_HORIZON_ECD * ECD_ANGLE_COEF_DJI) // pitch水平时电机的角度,0-360
 
 /* cmd应用包含的模块实例指针和交互信息存储*/
-static Publisher_t *chassis_cmd_pub;   // 底盘控制消息发布者
-static Subscriber_t *chassis_feed_sub; // 底盘反馈信息订阅者
+static Publisher_t* chassis_cmd_pub; // 底盘控制消息发布者
+static Subscriber_t* chassis_feed_sub; // 底盘反馈信息订阅者
 
-static Chassis_Ctrl_Cmd_s chassis_cmd_send;      // 发送给底盘应用的信息,包括控制信息和UI绘制相关
+static Chassis_Ctrl_Cmd_s chassis_cmd_send; // 发送给底盘应用的信息,包括控制信息和UI绘制相关
 static Chassis_Upload_Data_s chassis_fetch_data; // 从底盘应用接收的反馈信息信息,底盘功率枪口热量与底盘运动状态等
 
-static RC_ctrl_t *rc_data;              // 遥控器数据,初始化时返回
-static Vision_Recv_s *vision_recv_data; // 视觉接收数据指针,初始化时返回
+static RC_ctrl_t* rc_data; // 遥控器数据,初始化时返回
+static Vision_Recv_s* vision_recv_data; // 视觉接收数据指针,初始化时返回
 
-static Publisher_t *gimbal_cmd_pub;            // 云台控制消息发布者
-static Subscriber_t *gimbal_feed_sub;          // 云台反馈信息订阅者
-static Gimbal_Ctrl_Cmd_s gimbal_cmd_send;      // 传递给云台的控制信息
+static Publisher_t* gimbal_cmd_pub; // 云台控制消息发布者
+static Subscriber_t* gimbal_feed_sub; // 云台反馈信息订阅者
+static Gimbal_Ctrl_Cmd_s gimbal_cmd_send; // 传递给云台的控制信息
 static Gimbal_Upload_Data_s gimbal_fetch_data; // 从云台获取的反馈信息
 
-static Publisher_t *shoot_cmd_pub;           // 发射控制消息发布者
-static Subscriber_t *shoot_feed_sub;         // 发射反馈信息订阅者
-static Shoot_Ctrl_Cmd_s shoot_cmd_send;      // 传递给发射的控制信息
+static Publisher_t* shoot_cmd_pub; // 发射控制消息发布者
+static Subscriber_t* shoot_feed_sub; // 发射反馈信息订阅者
+static Shoot_Ctrl_Cmd_s shoot_cmd_send; // 传递给发射的控制信息
 static Shoot_Upload_Data_s shoot_fetch_data; // 从发射获取的反馈信息
 
 static Robot_Status_e robot_state; // 机器人整体工作状态
 
 static float AutomaticPitchControlRatio = 1.0f;
 
-void RobotCMDInit() {
-    rc_data = RemoteControlInit(&huart3);   // 修改为对应串口,注意如果是自研板dbus协议串口需选用添加了反相器的那个
+void RobotCMDInit()
+{
+    rc_data = RemoteControlInit(&huart3); // 修改为对应串口,注意如果是自研板dbus协议串口需选用添加了反相器的那个
     vision_recv_data = VisionInit(&huart1); // 视觉通信串口
 
     gimbal_cmd_pub = PubRegister("gimbal_cmd", sizeof(Gimbal_Ctrl_Cmd_s));
@@ -63,44 +62,45 @@ void RobotCMDInit() {
 /**
  * @brief 根据gimbal app传回的当前电机角度计算和零位的误差
  *        单圈绝对角度的范围是0~360,说明文档中有图示
- *
  */
-static void CalcOffsetAngle() {
+static void CalcOffsetAngle()
+{
     // 别名angle提高可读性,不然太长了不好看,虽然基本不会动这个函数
     static float angle;
     angle = gimbal_fetch_data.yaw_motor_single_round_angle; // 从云台获取的当前yaw电机单圈角度
-#if YAW_ECD_GREATER_THAN_4096                               // 如果大于180度
-    if (angle > YAW_ALIGN_ANGLE && angle <= 180.0f + YAW_ALIGN_ANGLE)
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE;
-    else if (angle > 180.0f + YAW_ALIGN_ANGLE)
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE - 360.0f;
-    else
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE;
-#else // 小于180度
-    if (angle > YAW_ALIGN_ANGLE)
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE;
-    else if (angle <= YAW_ALIGN_ANGLE && angle >= YAW_ALIGN_ANGLE - 180.0f)
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE;
-    else
-        chassis_cmd_send.offset_angle = angle - YAW_ALIGN_ANGLE + 360.0f;
+
+    float offset_angle = angle - YAW_ALIGN_ANGLE; // 计算初步偏差
+#if YAW_ECD_GREATER_THAN_4096
+    if (angle > 180.0f + YAW_ALIGN_ANGLE)
+        offset_angle -= 360.0f;
+#else
+    if (angle < YAW_ALIGN_ANGLE - 180.0f)
+        offset_angle += 360.0f;
 #endif
+    chassis_cmd_send.offset_angle = offset_angle;
 }
 
 /**
  * @brief 控制输入为遥控器(调试时)的模式和控制量设置
  *
  */
-static void RemoteControlSet() {
+static void RemoteControlSet()
+{
     chassis_cmd_send.chassis_mode = CHASSIS_NO_FOLLOW;
     gimbal_cmd_send.gimbal_mode = GIMBAL_GYRO_MODE;
     // 云台参数,确定云台控制数据
-    if (switch_is_up(rc_data[TEMP].rc.switch_left)) { //遥控器左侧开关在上，纯遥控器控制云台
-        gimbal_cmd_send.yaw -= 0.005f * (float) rc_data[TEMP].rc.rocker_l_;
-        gimbal_cmd_send.pitch -= 0.001f * (float) rc_data[TEMP].rc.rocker_l1;
+    if (switch_is_up(rc_data[TEMP].rc.switch_left))
+    {
+        //遥控器左侧开关在上，纯遥控器控制云台
+        gimbal_cmd_send.yaw -= 0.005f * (float)rc_data[TEMP].rc.rocker_l_;
+        gimbal_cmd_send.pitch -= 0.001f * (float)rc_data[TEMP].rc.rocker_l1;
 
-        chassis_cmd_send.vx = 10.0f * (float) rc_data[TEMP].rc.rocker_r_;
-        chassis_cmd_send.vy = 10.0f * (float) rc_data[TEMP].rc.rocker_r1;
-    } else if (switch_is_mid(rc_data[TEMP].rc.switch_left)) { //遥控器左侧开关在中间，视觉控制云台移动
+        chassis_cmd_send.vx = 10.0f * (float)rc_data[TEMP].rc.rocker_r_;
+        chassis_cmd_send.vy = 10.0f * (float)rc_data[TEMP].rc.rocker_r1;
+    }
+    else if (switch_is_mid(rc_data[TEMP].rc.switch_left))
+    {
+        //遥控器左侧开关在中间，视觉控制云台移动
         gimbal_cmd_send.yaw = vision_recv_data->yaw;
         gimbal_cmd_send.pitch = vision_recv_data->pitch;
 
@@ -109,23 +109,30 @@ static void RemoteControlSet() {
     }
 
     // 软件限位
-    if (gimbal_cmd_send.pitch < -30) {
+    if (gimbal_cmd_send.pitch < -30)
+    {
         gimbal_cmd_send.pitch = -30;
-    } else if (gimbal_cmd_send.pitch > 30) {
+    }
+    else if (gimbal_cmd_send.pitch > 30)
+    {
         gimbal_cmd_send.pitch = 30;
     }
 }
 
-static void AutomaticControlSet() {
+static void AutomaticControlSet()
+{
     chassis_cmd_send.chassis_mode = CHASSIS_ROTATE;
     gimbal_cmd_send.gimbal_mode = GIMBAL_GYRO_MODE;
 
     gimbal_cmd_send.yaw += 1.0f;
     gimbal_cmd_send.pitch += 1.0f * AutomaticPitchControlRatio;
-    if (gimbal_cmd_send.pitch < -30) {
+    if (gimbal_cmd_send.pitch < -30)
+    {
         gimbal_cmd_send.pitch = -30;
         AutomaticPitchControlRatio *= -1;
-    } else if (gimbal_cmd_send.pitch > 30) {
+    }
+    else if (gimbal_cmd_send.pitch > 30)
+    {
         gimbal_cmd_send.pitch = 30;
         AutomaticPitchControlRatio *= -1;
     }
@@ -135,9 +142,11 @@ static void AutomaticControlSet() {
  * @brief  遥控器离线后紧急停止
  *
  */
-static void EmergencyHandler() {
+static void EmergencyHandler()
+{
     // 遥控器离线或者右侧开关状态[上]
-    if (!RemoteControlIsOnline() || switch_is_up(rc_data[TEMP].rc.switch_right) || robot_state == ROBOT_STOP) {
+    if (!RemoteControlIsOnline() || switch_is_up(rc_data[TEMP].rc.switch_right) || robot_state == ROBOT_STOP)
+    {
         robot_state = ROBOT_STOP;
         gimbal_cmd_send.gimbal_mode = GIMBAL_ZERO_FORCE;
         chassis_cmd_send.chassis_mode = CHASSIS_ZERO_FORCE;
@@ -147,25 +156,29 @@ static void EmergencyHandler() {
         LOGERROR("[CMD] emergency stop!");
     }
     // 遥控器在线且右侧开关状态不为[上]
-    if (RemoteControlIsOnline() && !switch_is_up(rc_data[TEMP].rc.switch_right)) {
+    if (RemoteControlIsOnline() && !switch_is_up(rc_data[TEMP].rc.switch_right))
+    {
         robot_state = ROBOT_READY;
         LOGINFO("[CMD] reinstate, robot ready");
     }
 }
 
 /* 机器人核心控制任务,200Hz频率运行(必须高于视觉发送频率) */
-void RobotCMDTask() {
-    SubGetMessage(chassis_feed_sub, (void *) &chassis_fetch_data);
+void RobotCMDTask()
+{
+    SubGetMessage(chassis_feed_sub, (void*)&chassis_fetch_data);
     SubGetMessage(shoot_feed_sub, &shoot_fetch_data);
     SubGetMessage(gimbal_feed_sub, &gimbal_fetch_data);
 
     // 根据gimbal的反馈值计算云台和底盘正方向的夹角,不需要传参,通过static私有变量完成
     CalcOffsetAngle();
     // 根据遥控器左侧开关,确定当前使用的控制模式为遥控器调试还是键鼠
-    if (switch_is_mid(rc_data[TEMP].rc.switch_right)) {
+    if (switch_is_mid(rc_data[TEMP].rc.switch_right))
+    {
         // 遥控器右侧开关状态为[中]，遥控器控制
         RemoteControlSet();
-    } else if (switch_is_down(rc_data[TEMP].rc.switch_right)) // 遥控器右侧开关状态为[下]，自动控制
+    }
+    else if (switch_is_down(rc_data[TEMP].rc.switch_right)) // 遥控器右侧开关状态为[下]，自动控制
         AutomaticControlSet();
 
     EmergencyHandler(); // 处理模块离线和遥控器急停等紧急情况
@@ -179,7 +192,7 @@ void RobotCMDTask() {
     SlamSetAltitude(chassis_fetch_data.real_vx, chassis_fetch_data.real_vy, gimbal_fetch_data.gimbal_imu_data.Gyro[2]);
     VisionSetAltitude(gimbal_cmd_send.yaw, -gimbal_cmd_send.pitch, 0.0f, 1.0f);
 
-    PubPushMessage(chassis_cmd_pub, (void *) &chassis_cmd_send);
-    PubPushMessage(shoot_cmd_pub, (void *) &shoot_cmd_send);
-    PubPushMessage(gimbal_cmd_pub, (void *) &gimbal_cmd_send);
+    PubPushMessage(chassis_cmd_pub, (void*)&chassis_cmd_send);
+    PubPushMessage(shoot_cmd_pub, (void*)&shoot_cmd_send);
+    PubPushMessage(gimbal_cmd_pub, (void*)&gimbal_cmd_send);
 }
--- modules/remote/remote_control.c
+++ modules/remote/remote_control.c
@@ -9,12 +9,12 @@
 #define REMOTE_CONTROL_FRAME_SIZE 18u // 遥控器接收的buffer大小
 
 // 遥控器数据
-static RC_ctrl_t rc_ctrl[2];     //[0]:当前数据TEMP,[1]:上一次的数据LAST.用于按键持续按下和切换的判断
+static RC_ctrl_t rc_ctrl[2]; //[0]:当前数据TEMP,[1]:上一次的数据LAST.用于按键持续按下和切换的判断
 static uint8_t rc_init_flag = 0; // 遥控器初始化标志位
 
 // 遥控器拥有的串口实例,因为遥控器是单例,所以这里只有一个,就不封装了
-static USARTInstance *rc_usart_instance;
-static DaemonInstance *rc_daemon_instance;
+static USARTInstance* rc_usart_instance;
+static DaemonInstance* rc_daemon_instance;
 
 /**
  * @brief 矫正遥控器摇杆的值,超过660或者小于-660的值都认为是无效值,置0
@@ -32,27 +32,30 @@ static void RectifyRCjoystick()
  *
  * @param sbus_buf 接收buffer
  */
-static void sbus_to_rc(const uint8_t *sbus_buf)
+static void sbus_to_rc(const uint8_t* sbus_buf)
 {
     // 摇杆,直接解算时减去偏置
-    rc_ctrl[TEMP].rc.rocker_r_ = ((sbus_buf[0] | (sbus_buf[1] << 8)) & 0x07ff) - RC_CH_VALUE_OFFSET;                              //!< Channel 0
-    rc_ctrl[TEMP].rc.rocker_r1 = (((sbus_buf[1] >> 3) | (sbus_buf[2] << 5)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 1
-    rc_ctrl[TEMP].rc.rocker_l_ = (((sbus_buf[2] >> 6) | (sbus_buf[3] << 2) | (sbus_buf[4] << 10)) & 0x07ff) - RC_CH_VALUE_OFFSET; //!< Channel 2
-    rc_ctrl[TEMP].rc.rocker_l1 = (((sbus_buf[4] >> 1) | (sbus_buf[5] << 7)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 3
-    rc_ctrl[TEMP].rc.dial = ((sbus_buf[16] | (sbus_buf[17] << 8)) & 0x07FF) - RC_CH_VALUE_OFFSET;                                 // 左侧拨轮
+    rc_ctrl[TEMP].rc.rocker_r_ = ((sbus_buf[0] | (sbus_buf[1] << 8)) & 0x07ff) - RC_CH_VALUE_OFFSET; //!< Channel 0
+    rc_ctrl[TEMP].rc.rocker_r1 = (((sbus_buf[1] >> 3) | (sbus_buf[2] << 5)) & 0x07ff) - RC_CH_VALUE_OFFSET;
+    //!< Channel 1
+    rc_ctrl[TEMP].rc.rocker_l_ = (((sbus_buf[2] >> 6) | (sbus_buf[3] << 2) | (sbus_buf[4] << 10)) & 0x07ff) -
+        RC_CH_VALUE_OFFSET; //!< Channel 2
+    rc_ctrl[TEMP].rc.rocker_l1 = (((sbus_buf[4] >> 1) | (sbus_buf[5] << 7)) & 0x07ff) - RC_CH_VALUE_OFFSET;
+    //!< Channel 3
+    rc_ctrl[TEMP].rc.dial = ((sbus_buf[16] | (sbus_buf[17] << 8)) & 0x07FF) - RC_CH_VALUE_OFFSET; // 左侧拨轮
     RectifyRCjoystick();
     // 开关,0左1右
-    rc_ctrl[TEMP].rc.switch_right = ((sbus_buf[5] >> 4) & 0x0003);     //!< Switch right
+    rc_ctrl[TEMP].rc.switch_right = ((sbus_buf[5] >> 4) & 0x0003); //!< Switch right
     rc_ctrl[TEMP].rc.switch_left = ((sbus_buf[5] >> 4) & 0x000C) >> 2; //!< Switch left
 
     // 鼠标解析
     rc_ctrl[TEMP].mouse.x = (sbus_buf[6] | (sbus_buf[7] << 8)); //!< Mouse X axis
     rc_ctrl[TEMP].mouse.y = (sbus_buf[8] | (sbus_buf[9] << 8)); //!< Mouse Y axis
-    rc_ctrl[TEMP].mouse.press_l = sbus_buf[12];                 //!< Mouse Left Is Press ?
-    rc_ctrl[TEMP].mouse.press_r = sbus_buf[13];                 //!< Mouse Right Is Press ?
+    rc_ctrl[TEMP].mouse.press_l = sbus_buf[12]; //!< Mouse Left Is Press ?
+    rc_ctrl[TEMP].mouse.press_r = sbus_buf[13]; //!< Mouse Right Is Press ?
 
     //  位域的按键值解算,直接memcpy即可,注意小端低字节在前,即lsb在第一位,msb在最后
-    *(uint16_t *)&rc_ctrl[TEMP].key[KEY_PRESS] = (uint16_t)(sbus_buf[14] | (sbus_buf[15] << 8));
+    *(uint16_t*)&rc_ctrl[TEMP].key[KEY_PRESS] = (uint16_t)(sbus_buf[14] | (sbus_buf[15] << 8));
     if (rc_ctrl[TEMP].key[KEY_PRESS].ctrl) // ctrl键按下
         rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL] = rc_ctrl[TEMP].key[KEY_PRESS];
     else
@@ -62,12 +65,12 @@ static void sbus_to_rc(const uint8_t *sbus_buf)
     else
         memset(&rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT], 0, sizeof(Key_t));
 
-    uint16_t key_now = rc_ctrl[TEMP].key[KEY_PRESS].keys,                   // 当前按键是否按下
-        key_last = rc_ctrl[LAST].key[KEY_PRESS].keys,                       // 上一次按键是否按下
-        key_with_ctrl = rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL].keys,        // 当前ctrl组合键是否按下
-        key_with_shift = rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT].keys,      //  当前shift组合键是否按下
-        key_last_with_ctrl = rc_ctrl[LAST].key[KEY_PRESS_WITH_CTRL].keys,   // 上一次ctrl组合键是否按下
-        key_last_with_shift = rc_ctrl[LAST].key[KEY_PRESS_WITH_SHIFT].keys; // 上一次shift组合键是否按下
+    uint16_t key_now = rc_ctrl[TEMP].key[KEY_PRESS].keys, // 当前按键是否按下
+             key_last = rc_ctrl[LAST].key[KEY_PRESS].keys, // 上一次按键是否按下
+             key_with_ctrl = rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL].keys, // 当前ctrl组合键是否按下
+             key_with_shift = rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT].keys, //  当前shift组合键是否按下
+             key_last_with_ctrl = rc_ctrl[LAST].key[KEY_PRESS_WITH_CTRL].keys, // 上一次ctrl组合键是否按下
+             key_last_with_shift = rc_ctrl[LAST].key[KEY_PRESS_WITH_SHIFT].keys; // 上一次shift组合键是否按下
 
     for (uint16_t i = 0, j = 0x1; i < 16; j <<= 1, i++)
     {
@@ -93,7 +96,7 @@ static void sbus_to_rc(const uint8_t *sbus_buf)
  */
 static void RemoteControlRxCallback()
 {
-    DaemonReload(rc_daemon_instance);         // 先喂狗
+    DaemonReload(rc_daemon_instance); // 先喂狗
     sbus_to_rc(rc_usart_instance->recv_buff); // 进行协议解析
 }
 
@@ -101,14 +104,14 @@ static void RemoteControlRxCallback()
  * @brief 遥控器离线的回调函数,注册到守护进程中,串口掉线时调用
  *
  */
-static void RCLostCallback(void *id)
+static void RCLostCallback(void* id)
 {
     memset(rc_ctrl, 0, sizeof(rc_ctrl)); // 清空遥控器数据
     USARTServiceInit(rc_usart_instance); // 尝试重新启动接收
     LOGWARNING("[rc] remote control lost");
 }
 
-RC_ctrl_t *RemoteControlInit(UART_HandleTypeDef *rc_usart_handle)
+RC_ctrl_t* RemoteControlInit(UART_HandleTypeDef* rc_usart_handle)
 {
     USART_Init_Config_s conf;
     conf.module_callback = RemoteControlRxCallback;
